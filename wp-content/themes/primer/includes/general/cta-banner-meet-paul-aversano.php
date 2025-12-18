117c5343d3719d259efea433e759c5b47c5efaa7
Jim Poulos <jpoulos@pulse-creative.com>
2025-12-18 13:20:37 -0500

    new image for meet paul banner

--- wp-content/themes/primer/includes/general/cta-banner-meet-paul-aversano.php
+++ wp-content/themes/primer/includes/general/cta-banner-meet-paul-aversano.php
@@ -7,37 +7,37 @@
                 <figure class="d-flex flex-column justify-content-between">
                     <blockquote class="text-white">
                         <p class="text-white">
-                        <?php echo get_field('meet_paul_cta_quote');?>
+                            <?php echo get_field('meet_paul_cta_quote'); ?>
 
                         </p>
 
 
                     </blockquote>
                     <figcaption class="text-uppercase">
-                                <span class="name text-white"> <span
-                                        class="fist-name text-white"><?php echo get_field('first_name', 2933);?>&nbsp;</span>
-                                    <span class="last-name text-white"><?php echo get_field('last_name', 2933);?></span>
-                                </span>
-                                <span class="title-gl-area text-white">,
-                                    <span class="title text-white"><?php echo get_field('title', 2933);?></span>
-                                    <span class="gl-area text-white"><?php echo get_field('gl_area', 2933);?></span>
-                                </span>
+                        <span class="name text-white"> <span
+                                class="fist-name text-white"><?php echo get_field('first_name', 2933); ?>&nbsp;</span>
+                            <span class="last-name text-white"><?php echo get_field('last_name', 2933); ?></span>
+                        </span>
+                        <span class="title-gl-area text-white">,
+                            <span class="title text-white"><?php echo get_field('title', 2933); ?></span>
+                            <span class="gl-area text-white"><?php echo get_field('gl_area', 2933); ?></span>
+                        </span>
 
 
-                            </figcaption>
+                    </figcaption>
                 </figure>
-                <a href="<?php echo get_field('external_link', 2933);?>" class="cta-btn mt-2" targe="_blank">
+                <a href="<?php echo get_field('external_link', 2933); ?>" class="cta-btn mt-2" targe="_blank">
                     <div class="cta-inner cta-inner--white d-flex align-items-center mb-0"><span
                             class="arrow_carrot-right"></span><span class="btn-label">Meet Paul Aversano</span>
                     </div>
                 </a>
             </div>
             <div class="col-12 px-0 col-md-4 text-left pl-md-5 order-1 order-md-2">
-                <img class="paul-headshot d-md-none" src="<?php echo get_template_directory_uri(); ?>/library/images/reboot/our-team/paul_aversano_landscape_0.png); " alt="<?php echo get_field('first_name', 2933);?> <?php echo get_field('last_name', 2933);?>">
+                <img class="paul-headshot d-md-none img-fluid" src="<?php echo site_url(); ?>/wp-content/uploads/2025/12/paul_aversano_landscape_0_new@1.5x.png); " alt="<?php echo get_field('first_name', 2933); ?> <?php echo get_field('last_name', 2933); ?>" width="289" height="auto">
 
             </div>
         </div>
-        <img class="paul-headshot d-none d-md-block" src="<?php echo get_template_directory_uri(); ?>/library/images/reboot/our-team/paul_aversano_landscape_0.png); " alt="<?php echo get_field('first_name', 2933);?> <?php echo get_field('last_name', 2933);?>">
+        <img class="paul-headshot d-none d-md-block img-fluid" src="<?php echo site_url(); ?>/wp-content/uploads/2025/12/paul_aversano_landscape_0_new@1.5x.png); " alt="<?php echo get_field('first_name', 2933); ?> <?php echo get_field('last_name', 2933); ?>" width="289" height="auto">
 
     </div>
 
